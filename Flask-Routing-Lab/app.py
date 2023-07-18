c83d206e744a4b4cfdf5d7474bd311950c35c5d0
odedbe24-meet <106547160+odedbe24-meet@users.noreply.github.com>
2023-07-18 15:49:47 +0300

    First Time Commiting

--- Flask-Routing-Lab/app.py
+++ Flask-Routing-Lab/app.py
@@ -9,9 +9,18 @@ app = Flask(  # Create a flask app
 
 # Your code should be below
 
+@app.route('/')
+def Home():
+    return render_template('home.html')
+
+@app.route('/product')
+def Product():
+    return render_template('product.html')
 
 
-
+@app.route('/cart')
+def Cart():
+    return render_template('cart.html')
 # Your code should be above
 
 if __name__ == "__main__":  # Makes sure this is the main process
